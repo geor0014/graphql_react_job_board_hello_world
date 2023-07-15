536ab252f00c58a70d361bddbb08c9df143413d8
geor0014 <geor0014@hz.nl>
2023-07-15 17:48:35 +0200

    5 - add job and company association in schema.graphql and resolvers.js (server)

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,12 @@
-import { Job } from "./db.js";
+import { Job, Company } from "./db.js";
 
 export const resolvers = {
   Query: {
     jobs: async () => Job.findAll(),
+  },
+
+  Job: {
+    //   in this case the parent is each job object in the array of jobs returned from the resolver for the jobs query
+    company: async (parent) => Company.findById(parent.companyId),
   },
 };
