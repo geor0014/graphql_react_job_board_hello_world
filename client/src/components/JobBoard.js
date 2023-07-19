6f526bdb7fcad65e05276df437a1053a042ff6f0
geor0014 <geor0014@hz.nl>
2023-07-19 09:30:12 +0200

    28 - create a hooks.js file in the graphql folder to hold custom hooks for the graphql queries

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -1,15 +1,11 @@
+import { useJobs } from "../graphql/hooks";
 import JobList from "./JobList";
-import { JOBS_QUERY } from "../graphql/queries";
-import { useQuery } from "@apollo/client";
 
 function JobBoard() {
-  const { data, loading, error } = useQuery(JOBS_QUERY, {
-    fetchPolicy: "network-only",
-  });
+  const { jobs, loading, error } = useJobs();
 
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>Something went wrong!</h1>;
-  const { jobs } = data;
   return (
     <div>
       <h1 className="title">Job Board</h1>
